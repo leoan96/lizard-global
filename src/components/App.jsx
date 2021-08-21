d8df5a0caf4c43a0a17f1e4a87b66169c8cc61f7
Mun Kit <leoan96@hotmail.com>
2021-08-21 12:17:40 +0800

    retrieve data from mock API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
-}
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  let [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/posts')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json.posts);
+        setPosts(json.posts);
+      });
+  }, []);
+
+  return <div></div>;
+};
 
 export default App;
