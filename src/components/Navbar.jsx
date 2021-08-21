db923c121ce0d5149d4838d843c4280c72d19442
Mun Kit <leoan96@hotmail.com>
2021-08-22 00:00:08 +0800

    display filter options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Checkbox from './Checkbox';
 
-const Navbar = () => {
+const Navbar = ({ filterByList, onFilterSubmit }) => {
   return (
-    <nav>
-      <div className="nav-wrapper container">
-        <Link to={'/'} className="brand-logo">
-          Lizard Blog
-        </Link>
-      </div>
-    </nav>
+    <React.Fragment>
+      <nav>
+        <div className="nav-wrapper container">
+          <Link to={'/'} className="brand-logo">
+            Lizard Blog
+          </Link>
+        </div>
+        <form action="#">
+          <label>
+            <h4>Filter By:</h4>
+          </label>
+
+          <Checkbox filterByList={filterByList} />
+          <p>
+            <input type="submit" value="Submit" onClick={onFilterSubmit} />
+          </p>
+        </form>
+      </nav>
+    </React.Fragment>
   );
 };
 
