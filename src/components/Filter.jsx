8c781c3adb32fc0546911c7abf77badba8926866
Mun Kit <leoan96@hotmail.com>
2021-08-22 17:50:54 +0800

    implement basic filter for posts

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 const Filter = ({ filter }) => {
   return (
     <p>
-      <label>
-        <input type="checkbox" className="filled-in" />
+      <label htmlFor={filter}>
+        <input
+          type="checkbox"
+          id={filter}
+          className="filled-in"
+          name={filter}
+          value={filter}
+        />
         <span>{filter}</span>
       </label>
     </p>
